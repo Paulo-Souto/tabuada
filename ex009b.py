612363b354eeea2ce31f0403680234341f1bed8e
Paulo Souto <146046315+Paulo-Souto@users.noreply.github.com>
2024-02-14 00:14:57 -0300

    Add files via upload
    Tabuada que fiz no meu aprendizado de Python.

---
+++ ex009b.py
@@ -0,0 +1,68 @@
+# Faça um programa que leia um número inteira qualquer e mostre na tela a sua tabuada (desafio a mais)
+tabuada = int(input('Digite um número para ver a sua tabuada: '))
+print('—' * 14)
+print(f'{tabuada} + 1 = {tabuada + 1}\n'
+      f'{tabuada} + 2 = {tabuada + 2}\n'
+      f'{tabuada} + 3 = {tabuada + 3}\n'
+      f'{tabuada} + 4 = {tabuada + 4}\n'
+      f'{tabuada} + 5 = {tabuada + 5}\n'
+      f'{tabuada} + 6 = {tabuada + 6}\n'
+      f'{tabuada} + 7 = {tabuada + 7}\n'
+      f'{tabuada} + 8 = {tabuada + 8}\n'
+      f'{tabuada} + 9 = {tabuada + 9}\n'
+      f'{tabuada} + 10 = {tabuada + 10}')
+print('—' * 14)
+
+print('—' * 14)
+print(f'{tabuada} - 1 = {tabuada - 1}\n'
+      f'{tabuada} - 2 = {tabuada - 2}\n'
+      f'{tabuada} - 3 = {tabuada - 3}\n'
+      f'{tabuada} - 4 = {tabuada - 4}\n'
+      f'{tabuada} - 5 = {tabuada - 5}\n'
+      f'{tabuada} - 6 = {tabuada - 6}\n'
+      f'{tabuada} - 7 = {tabuada - 7}\n'
+      f'{tabuada} - 8 = {tabuada - 8}\n'
+      f'{tabuada} - 9 = {tabuada - 9}\n'
+      f'{tabuada} - 10 = {tabuada - 10}')
+print('—' * 14)
+
+print('—' * 14)
+print(f'{tabuada} * 1 = {tabuada * 1}\n'
+      f'{tabuada} * 2 = {tabuada * 2}\n'
+      f'{tabuada} * 3 = {tabuada * 3}\n'
+      f'{tabuada} * 4 = {tabuada * 4}\n'
+      f'{tabuada} * 5 = {tabuada * 5}\n'
+      f'{tabuada} * 6 = {tabuada * 6}\n'
+      f'{tabuada} * 7 = {tabuada * 7}\n'
+      f'{tabuada} * 8 = {tabuada * 8}\n'
+      f'{tabuada} * 9 = {tabuada * 9}\n'
+      f'{tabuada} * 10 = {tabuada * 10}')
+
+print('—' * 14)
+
+print('—' * 14)
+print(f'{tabuada} / 1 = {tabuada / 1}\n'
+      f'{tabuada} / 2 = {tabuada / 2:.2f}\n'
+      f'{tabuada} / 3 = {tabuada / 3:.2f}\n'
+      f'{tabuada} / 4 = {tabuada / 4:.2f}\n'
+      f'{tabuada} / 5 = {tabuada / 5:.2f}\n'
+      f'{tabuada} / 6 = {tabuada / 6:.2f}\n'
+      f'{tabuada} / 7 = {tabuada / 7:.2f}\n'
+      f'{tabuada} / 8 = {tabuada / 8:.2f}\n'
+      f'{tabuada} / 9 = {tabuada / 9:.2f}\n'
+      f'{tabuada} / 10 = {tabuada / 10:.2f}')
+print('—' * 14)
+
+print('—' * 14)
+print(f'{tabuada} ** 0 = {tabuada ** 0}\n'
+      f'{tabuada} ** 1 = {tabuada ** 1}\n'
+      f'{tabuada} ** 2 = {tabuada ** 2}\n'
+      f'{tabuada} ** 3 = {tabuada ** 3}\n'
+      f'{tabuada} ** 4 = {tabuada ** 4}\n'
+      f'{tabuada} ** 5 = {tabuada ** 5}\n'
+      f'{tabuada} ** 6 = {tabuada ** 6}\n'
+      f'{tabuada} ** 7 = {tabuada ** 7}\n'
+      f'{tabuada} ** 8 = {tabuada ** 8}\n'
+      f'{tabuada} ** 9 = {tabuada ** 9}\n'
+      f'{tabuada} ** 10 = {tabuada ** 10}')
+print('—' * 14)
